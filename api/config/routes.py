5c80ae97ad5a9cefcd851a684ce115d8febdeedb
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-11-21 15:52:16 +0300

    update routes file

--- api/config/routes.py
+++ api/config/routes.py
@@ -5,7 +5,7 @@ import jwt
 from functools import wraps
 from flask import request, jsonify
 from api.config.config import Security
-from api.controllers.controller import UserController, OrderController
+from api.controllers.controller import Controller
 
 
 class Routes:
@@ -13,8 +13,7 @@ class Routes:
     Create a Routes class
     """
     def __init__(self):
-        self.user = UserController()
-        self.order = OrderController()
+        self.controller = Controller()
 
     def fetch_routes(self, app):
         """
@@ -41,50 +40,50 @@ class Routes:
 
         @app.route('/api/v1/auth/login', methods=['POST'], strict_slashes=False)
         def login():
-                return self.user.login()
+                return self.controller.login()
 
         @app.route('/api/v1/auth/signup', methods=['POST'], strict_slashes=False)
         def signup():
-            return self.user.signup()
+            return self.controller.signup()
 
         @app.route('/api/v1/users', methods=['GET'], strict_slashes=False)
         @required
         def get_users():
-            return self.user.get_users()
+            return self.controller.get_users()
             
         @app.route('/api/v1/parcels', methods=['POST', 'GET'], strict_slashes=False)
         @required
         def parcels():
             if request.method == 'POST':
-                return self.order.create_order()
-            return self.order.get_order()
+                return self.controller.create_order()
+            return self.controller.get_order()
 
         @app.route('/api/v1/parcels/<int:parcel_id>', methods=['GET'], strict_slashes=False)
         @required
         def get_single_parcel(parcel_id):
-            return self.order.get_order(parcel_id)
+            return self.controller.get_order(parcel_id)
 
         @app.route('/api/v1/users/<int:user_id>/parcels', methods=['GET'], strict_slashes=False)
         @required
         def get_parcel_user(user_id):
-            return self.order.get_order_user(user_id)
+            return self.controller.get_order_user(user_id)
 
         @app.route('/api/v1/parcels/<int:parcel_id>/cancel', methods=['PUT'], strict_slashes=False)
         @required
         def cancel_parcel(parcel_id):
-            return self.order.cancel_parcel(parcel_id)
+            return self.controller.change_product(parcel_id, 'user', 'order_status', 'cancel')
 
         @app.route('/api/v1/parcels/<int:parcel_id>/destination', methods=['PUT'], strict_slashes=False)
         @required
         def change_parcel_destination(parcel_id):
-            return self.order.change_destination(parcel_id)
+            return self.controller.change_product(parcel_id, 'user', 'order_status', 'destination')
 
         @app.route('/api/v1/parcels/<int:parcel_id>/status', methods=['PUT'], strict_slashes=False)
         @required
         def change_status(parcel_id):
-            return self.order.change_status(parcel_id)
+            return self.controller.change_product(parcel_id, 'admin', 'order_status', 'status')
 
         @app.route('/api/v1/parcels/<int:parcel_id>/presentLocation', methods=['PUT'], strict_slashes=False)
         @required
         def change_location(parcel_id):
-            return self.order.change_present_location(parcel_id)
+            return self.controller.change_product(parcel_id, 'admin', 'present', 'present')
