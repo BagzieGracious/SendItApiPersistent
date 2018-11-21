33084f45fa38bc7711a2bbe35fd82b10ef2535f0
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-11-21 10:35:19 +0300

    add the user model file

--- api/models/user.py
+++ api/models/user.py
@@ -1,80 +1,60 @@
-import jwt
-import datetime
-from flask import jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
 from api.config.database import Database
 
 
 class Users:
-    userLst = []
 
-    def __init__(self, *args):
-        self.user = args[0]
-        self.first = args[1]
-        self.last = args[2]
-        self.cont = args[3]
-        self.email = args[4]
-        self.pswd = args[5]
-        self.type = args[6]
-        self.database = Database().create_users_table()
+    def __init__(self):
+        self.connect = Database().connect
+        self.cursor = Database().cursor
 
-    def add_user(self):
+    def add_user(self, data):
         """
         Method for adding a user to data structures
         """
-        if Database().check_user_exists(self.user, self.email):
-            return jsonify({"status": "failure", "error": {"message": "user already exists"}}), 409
-        Database().add_user(self.user, self.first, self.last, self.last, self.email, self.pswd, self.type)
-        return jsonify({"status": "success", "message": "you have signed up successfully"}), 200
+        pwd = generate_password_hash(data['password'], method='sha256')
+        sql = """
+                INSERT INTO users(username, firstname, lastname, contact, email, password, usertype
+                )VALUES(%s, %s, %s, %s, %s, %s, %s)
+                """
+        self.cursor.execute(sql, (data['username'], data['firstname'],
+                                  data['lastname'], data['contact'], data['email'], pwd, data['usertype']))
 
-    @staticmethod
-    def check_credentials(username, password):
+    def check_credentials(self, username, password):
         """
         Method for checking users username or password
         """
-        if Database().authorize_user(username, password):
-            data = Database().user_data_username(username)
-            token = jwt.encode({
-                "user": {"user_id": data[0], "usertype": data[7]},
-                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-            }, 'json_web_token')
-            return jsonify({"status": "success", "message": "you have logged in successfully", "token": token.decode('UTF-8')}), 200
-        return jsonify({"status": "failure", "error": {"message": "invalid username or password"}}), 401
+        sql = "SELECT * FROM users WHERE username='{}'".format(username)
+        self.cursor.execute(sql)
+        data = self.cursor.fetchone()
+        if data is None:
+            return False
+        if check_password_hash(data[6], password):
+            return data
+        return False
 
-    @staticmethod
-    def get_all_users():
+    def get_all_users(self):
         """
         Method for getting all users from data structures
         """
-        Users.userLst.clear()
-        if Database().get_all_users() is None:
-            return jsonify({"status": "failure", "error": {"message": "no user found"}}), 404
-
-        for user in Database().get_all_users():
-            usr = {
-                'user_id': user[0],
-                'username': user[1],
-                'firstname': user[2],
-                'lastname': user[3],
-                'contact': user[4],
-                'email': user[5],
-                'usertype': user[7]
-            }
-            Users.userLst.append(usr)
-        return jsonify({"status": "success", "data": Users.userLst}), 200
+        sql = "SELECT * FROM users"
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
     def check_user_details(self, username, email):
         """
         Method for checking users details
         """
-        for user in Users.users:
-            if user['username'] == username or user['email'] == email:
-                return jsonify({"status": "failure", "error": {"message": "username or email already exists"}}), 403
+        sql = "SELECT * FROM users WHERE username='{}' OR email = '{}'".format(username, email)
+        self.cursor.execute(sql)
+        if self.cursor.fetchone():
+            return True
         return False
 
     def get_user_id(self, username):
         """
         Method that return user_id
         """
-        for user in Users.users:
-            if user['username'] == username:
-                return user['user_id']
+        sql = "SELECT * FROM users WHERE username='{}'".format(username)
+        self.cursor.execute(sql)
+        return self.cursor.fetchone()
