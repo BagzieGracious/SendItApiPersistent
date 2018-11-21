1fa3a77b6f3c8cf268060c7a8924f2a43e620d9b
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-11-21 20:07:49 +0300

    update data validation file

--- api/config/data_validation.py
+++ api/config/data_validation.py
@@ -1,6 +1,7 @@
 """
 Module to return validations values
 """
+from validate_email import validate_email
 from flask import jsonify
 
 
@@ -10,7 +11,7 @@ class DataValidation:
     """
     error_msg = {"status": 'failure', "error":{"message":""}}
 
-    def __init__(self, req, type):
+    def __init__(self, req):
         """
         Constructor for initializing datavaldation class
         """
@@ -25,41 +26,56 @@ class DataValidation:
         DataValidation.error_msg['error']['message'] = msg
         return DataValidation.error_msg
 
-    def string_check(self, lst):
-        """
-        Method for checking strings criteria
-        """
+    def type_check(self, lst, type):
         for error in lst:
-            if not isinstance(error, str):
-                return jsonify(self.err('username, email, and password should be a string')), 400
+            if not isinstance(error, type):
+                return jsonify(self.err("you have a {} error check your inputs".format(type))), 400
         return True
 
-    def validate(self):
-        """
-        Method for validating inputs
-        """
-        if self.type == 'user':
-            if self.req.content_type == 'application/json':
-                key = ['email', 'username', 'password']
-                if set(key).issubset(self.data):
-                    if isinstance(self.data['email'], str) and isinstance(self.data['username'], str) and isinstance(self.data['password'], str):
-                        if self.data['email'] != '' and self.data['username'] != '' and self.data['password'] != '':
-                            return True
-                        return jsonify(self.err('no field should be empty')), 400
-                    return jsonify(self.err('email, username and password should be a string')), 400
-                return jsonify(self.err('some field is missing (email, password, username)')), 400
-            return jsonify(self.err('only json data is allowed')), 400
+    def empty_check(self, lst):
+        for error in lst:
+            if error == '':
+                return jsonify(self.err("no empty field allowed")), 400
+        return True
 
-        if self.type == 'order':
-            if self.req.content_type == 'application/json':
-                key = ['pickup', 'destination', 'description', 'weight', 'product']
-                if set(key).issubset(self.data):
-                    if isinstance(self.data['weight'], int) and self.data['weight'] > 0:
-                        if isinstance(self.data['pickup'], str) and isinstance(self.data['description'], str) and isinstance(self.data['destination'], str) and isinstance(self.data['product'], str):
-                            if self.data['pickup'] != '' and self.data['description'] != '' and self.data['destination'] and self.data['product'] != '' and self.data['weight'] != '':
-                                return True
-                            return jsonify(self.err('no field should be empty')), 400
-                        return jsonify(self.err('pickup, destination, description, and product should be a string')), 400
-                    return jsonify(self.err('weight should be an integer and above 0')), 400
-                return jsonify(self.err('some field is missing')), 400
-            return jsonify(self.err('only json data is allowed')), 400
+    def email_check(self, email):
+        if validate_email(email):
+            return True
+        return jsonify(self.err("invalid email"))
+
+    def password_check(self, password):
+        if len(password) >= 6:
+            return True
+        return jsonify(self.err("password should be greater that 5 chars"))
+
+    def key_check(self, lst, request):
+        if set(lst).issubset(request):
+            return True
+        return jsonify(self.err('some field is missing')), 400
+
+    def validator(self, lst, type):
+        if self.req.content_type == 'application/json':
+            if self.key_check(lst, self.data):
+                if type == 'signup':
+                    err = [
+                        self.type_check([self.data['username'], self.data['firstname'], self.data['contact'], self.data['email'], self.data['password']], str), 
+                        self.empty_check([self.data['username'], self.data['firstname'], self.data['contact'], self.data['email'], self.data['password']]), 
+                        self.email_check(self.data['email']), 
+                        self.password_check(self.data['password'])
+                    ]
+
+                if type == 'order':
+                    err = [
+                        self.type_check([self.data['product'], self.data['description'], self.data['pickup'], self.data['destination']], str),
+                        self.type_check([self.data['weight']], int),
+                        self.empty_check([self.data['product'], self.data['description'], self.data['pickup'], self.data['destination'], self.data['weight']])
+                    ]
+
+
+                for value in err:
+                    if not isinstance(value, bool):
+                        return value
+                return True
+            return key_check(lst, self.data)
+        return jsonify(self.err('only json data is allowed')), 400   
+        
