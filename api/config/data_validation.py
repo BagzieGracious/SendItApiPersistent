3a9f3cd49ddad3c9b06965ede09bb4ba95a3d21a
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-11-22 08:42:40 +0300

    update data validation files, with the neccessary validations like email

--- api/config/data_validation.py
+++ api/config/data_validation.py
@@ -1,8 +1,9 @@
 """
 Module to return validations values
 """
-from validate_email import validate_email
 from flask import jsonify
+import re
+from validate_email import validate_email
 
 
 class DataValidation:
@@ -17,7 +18,6 @@ class DataValidation:
         """
         self.req = req
         self.data = self.req.get_json()
-        self.type = type
 
     def err(self, msg):
         """
@@ -27,41 +27,61 @@ class DataValidation:
         return DataValidation.error_msg
 
     def type_check(self, lst, type):
+        """
+        Method that checks if each value in a list of a certain type
+        """
         for error in lst:
             if not isinstance(error, type):
                 return jsonify(self.err("you have a {} error check your inputs".format(type))), 400
         return True
 
     def empty_check(self, lst):
+        """
+        Mehtod that checks if each element is in a list is empty
+        """
         for error in lst:
             if error == '':
                 return jsonify(self.err("no empty field allowed")), 400
         return True
 
     def email_check(self, email):
+        """
+        Method that checks if the email is valid
+        """
         if validate_email(email):
             return True
-        return jsonify(self.err("invalid email"))
+        return jsonify(self.err("invalid email")), 406
 
     def password_check(self, password):
+        """
+        Method that checks if the password is greater than 5 chars
+        """
         if len(password) >= 6:
             return True
-        return jsonify(self.err("password should be greater that 5 chars"))
+        return jsonify(self.err("password should be greater that 5 chars")), 406
 
-    def key_check(self, lst, request):
-        if set(lst).issubset(request):
+    def contact_check(self, contact):
+        """
+        Method that checks if the contact is correct
+        """
+        rgx = re.compile("^[0-9]{10,13}$")
+        if rgx.search(contact):
             return True
-        return jsonify(self.err('some field is missing')), 400
+        return jsonify(self.err("Invalid contact format")), 406
 
     def validator(self, lst, type):
+        """
+        Method that validates the request object
+        """
         if self.req.content_type == 'application/json':
-            if self.key_check(lst, self.data):
+            if set(lst).issubset(self.data):
                 if type == 'signup':
                     err = [
                         self.type_check([self.data['username'], self.data['firstname'], self.data['contact'], self.data['email'], self.data['password']], str), 
                         self.empty_check([self.data['username'], self.data['firstname'], self.data['contact'], self.data['email'], self.data['password']]), 
                         self.email_check(self.data['email']), 
-                        self.password_check(self.data['password'])
+                        self.password_check(self.data['password']),
+                        self.contact_check(self.data['contact'])
                     ]
 
                 if type == 'order':
@@ -71,11 +91,28 @@ class DataValidation:
                         self.empty_check([self.data['product'], self.data['description'], self.data['pickup'], self.data['destination'], self.data['weight']])
                     ]
 
-
                 for value in err:
                     if not isinstance(value, bool):
                         return value
                 return True
-            return key_check(lst, self.data)
-        return jsonify(self.err('only json data is allowed')), 400   
-        
+            return jsonify(self.err("some field is missing")), 400
+        return jsonify(self.err('only json data is allowed')), 406
+
+    @staticmethod
+    def validate_product_change(req, type):
+        """
+        Method that validates the update of products details
+        """
+        if req.content_type == 'application/json':
+            if type == 'cancel':
+                return True
+
+            data = req.get_json()
+            if set(['data']).issubset(data):
+                if type == 'status':
+                    if data['data'] == 'in-transit' or data['data'] == 'delivered':
+                        return True
+                    return jsonify({"status": 'failure', "error":{"message":"only in-transit or delivered status is allowed"}}), 400
+                return True
+            return jsonify({"status": 'failure', "error":{"message":"some field is missing"}}), 400
+        return jsonify({"status": 'failure', "error":{"message":"only json data is allowed"}}), 400
