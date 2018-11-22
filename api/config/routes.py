d3453493da2fc99c9bd5d1713ba120a456ce672d
Bagenda Deogracious <bagendadeogracious@gmail.com>
2018-11-22 09:33:51 +0300

    update routes file

--- api/config/routes.py
+++ api/config/routes.py
@@ -34,56 +34,67 @@ class Routes:
                 return func(*args, **kwargs)
             return decorated
 
+        #default view
         @app.route('/')
         def index():
             return "<h1>Welcome to send-it</h1> <p>the only place that you can feel safe with delivery orders</p>"
 
-        @app.route('/api/v1/auth/login', methods=['POST'], strict_slashes=False)
+        #login view
+        @app.route('/api/v2/auth/login', methods=['POST'], strict_slashes=False)
         def login():
                 return self.controller.login()
 
-        @app.route('/api/v1/auth/signup', methods=['POST'], strict_slashes=False)
+        #signup view
+        @app.route('/api/v2/auth/signup', methods=['POST'], strict_slashes=False)
         def signup():
             return self.controller.signup()
 
-        @app.route('/api/v1/users', methods=['GET'], strict_slashes=False)
+        #get users view
+        @app.route('/api/v2/users', methods=['GET'], strict_slashes=False)
         @required
         def get_users():
             return self.controller.get_users()
-            
-        @app.route('/api/v1/parcels', methods=['POST', 'GET'], strict_slashes=False)
+        
+        #create order or get orders view
+        @app.route('/api/v2/parcels', methods=['POST', 'GET'], strict_slashes=False)
         @required
         def parcels():
             if request.method == 'POST':
                 return self.controller.create_order()
             return self.controller.get_order()
 
-        @app.route('/api/v1/parcels/<int:parcel_id>', methods=['GET'], strict_slashes=False)
+        #get single order view
+        @app.route('/api/v2/parcels/<int:parcel_id>', methods=['GET'], strict_slashes=False)
         @required
         def get_single_parcel(parcel_id):
             return self.controller.get_order(parcel_id)
 
-        @app.route('/api/v1/users/<int:user_id>/parcels', methods=['GET'], strict_slashes=False)
+        #get order by user view
+        @app.route('/api/v2/users/<int:user_id>/parcels', methods=['GET'], strict_slashes=False)
         @required
         def get_parcel_user(user_id):
             return self.controller.get_order_user(user_id)
 
-        @app.route('/api/v1/parcels/<int:parcel_id>/cancel', methods=['PUT'], strict_slashes=False)
+        #cancel a specific order view
+        @app.route('/api/v2/parcels/<int:parcel_id>/cancel', methods=['PUT'], strict_slashes=False)
         @required
         def cancel_parcel(parcel_id):
             return self.controller.change_product(parcel_id, 'user', 'order_status', 'cancel')
 
-        @app.route('/api/v1/parcels/<int:parcel_id>/destination', methods=['PUT'], strict_slashes=False)
+        #change destination view
+        @app.route('/api/v2/parcels/<int:parcel_id>/destination', methods=['PUT'], strict_slashes=False)
         @required
         def change_parcel_destination(parcel_id):
-            return self.controller.change_product(parcel_id, 'user', 'order_status', 'destination')
+            return self.controller.change_product(parcel_id, 'user', 'destination', 'destination')
 
-        @app.route('/api/v1/parcels/<int:parcel_id>/status', methods=['PUT'], strict_slashes=False)
+        #change order status view
+        @app.route('/api/v2/parcels/<int:parcel_id>/status', methods=['PUT'], strict_slashes=False)
         @required
         def change_status(parcel_id):
             return self.controller.change_product(parcel_id, 'admin', 'order_status', 'status')
 
-        @app.route('/api/v1/parcels/<int:parcel_id>/presentLocation', methods=['PUT'], strict_slashes=False)
+        #change present location view
+        @app.route('/api/v2/parcels/<int:parcel_id>/presentLocation', methods=['PUT'], strict_slashes=False)
         @required
         def change_location(parcel_id):
             return self.controller.change_product(parcel_id, 'admin', 'present', 'present')
